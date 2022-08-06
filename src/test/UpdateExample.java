fbe453e200a30d0ebed3776627581339128f7a92
your name <you@example.com>
2022-08-06 12:06:59 +0530

    pushing database files

---
+++ src/test/UpdateExample.java
@@ -0,0 +1,31 @@
+package test;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class UpdateExample {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		 Class.forName("com.mysql.cj.jdbc.Driver");  
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "admin");
+			
+			Statement stm = con.createStatement();
+			stm.executeUpdate("update eproduct set price=2500.00 where name='tablet'");
+					
+					ResultSet result = stm.executeQuery("select * from eproduct");
+					
+					while(result.next()) {
+						
+						System.out.println(result.getInt("ID"));
+						System.out.println(result.getString("name"));
+						System.out.println(result.getFloat("price"));
+	}
+
+}
+}
